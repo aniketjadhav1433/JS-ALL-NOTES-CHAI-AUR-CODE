2489ac19e25829608f6285b9bd6e031c1590a473
aniketjadhav1433 <aniketjadhav1433@gmail.com>
2024-08-03 17:49:15 +0530

    add gitignore and events html & js

---
+++ events.js
@@ -0,0 +1,39 @@
+let images = document.querySelector(".images")
+let flower = document.querySelector(".image2")
+let google = document.querySelector(".google")
+
+
+// images.addEventListener("click", (e)=>{
+//     console.log('images clicked',);
+// },false)     // the third parameter false is deafult
+  
+// flower.addEventListener("click",(e)=>{
+//     console.log('flower clicked',);     //this will perform bubbling first we will see flower clicked and then images clicked bcz it goes from inside to outside
+// },false)
+
+
+
+// images.addEventListener("click", (e)=>{
+//     console.log('images clicked',);
+// },true)    
+
+// flower.addEventListener("click",(e)=>{      // now this will not perform bubbling it will come from outside to inside first images clicked will appear then flower clicked will appear
+//     console.log('flower clicked',);
+// },true)
+
+
+
+images.addEventListener("click", (e)=>{
+    console.log('images clicked',);
+})    
+
+flower.addEventListener("click",(e)=>{    
+    console.log('flower clicked',);
+    e.stopPropagation()            // by doing this now images clicked will not appear
+})
+
+google.addEventListener("click",(e)=>{
+    console.log('google clicked',);
+    e.stopPropagation()
+    e.preventDefault()          // this prevents the default behaviour of element , now this will not take us to the google website
+})
